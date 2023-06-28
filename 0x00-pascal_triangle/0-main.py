0579045d2eba5522be4f8bbf6f22b8f0c7869175
Maryswy <marysowa54@gmail.com>
2023-06-29 00:24:50 +0300

    Creating a function def pascal_triangle(n) that returns a list of lists of integers representing the Pascals triangle of n, Returns an empty list if n <= 0 and n is an integer (Pascals Triangle)

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -2,7 +2,8 @@
 """
 0-main
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+from 0-pascal_triangle import pascal_triangle
 
 def print_triangle(triangle):
     """
@@ -12,6 +13,5 @@ def print_triangle(triangle):
         print("[{}]".format(",".join([str(x) for x in row])))
 
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
-
+   if __name__ == "__main__":
+       print_triangle(pascal_triangle(5))
