b8b41199a05f1b5f4da0a3ebf148b218ce65ab28
Maryswy <marysowa54@gmail.com>
2023-07-23 00:55:31 +0300

    A script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python3
-"""
-reads stdin line by line and computes metrics
+"""  A script that Reads stdin line by line and computes metrics
 """
 import sys
 
+
 if __name__ == "__main__":
-
+    status = {"200": 0,
+              "301": 0,
+              "400": 0,
+              "401": 0,
+              "403": 0,
+              "404": 0,
+              "405": 0,
+              "500": 0}
+    count = 1
     file_size = 0
-    count = 0
-    sum_codes = {
-        '200': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '403': 0,
-        '405': 0,
-        '500': 0,
-    }
 
-    def print_codes(sum_codes, size):
-        """
-        print info
-        """
-        print('File size: {}'.format(size))
-        for code, count in sorted(sum_codes.items()):
-            if count > 0:
-                print('{}: {}'.format(code, count))
+    def get_line(line):
+        """ parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in status.keys():
+                status[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats"""
+        print("File size: {}".format(file_size))
+        for key in sorted(status.keys()):
+            if status[key]:
+                print("{}: {}".format(key, status[key]))
 
     try:
         for line in sys.stdin:
-            count += 1
-            input_array = line.split()
-            try:
-                file_size += int(input_array[-1])
-                code = input_array[-2]
-                if code in sum_codes:
-                    sum_codes[code] += 1
-            except Exception:
-                pass
+            file_size += get_line(line)
             if count % 10 == 0:
-                print_codes(sum_codes, file_size)
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
-        print_codes(sum_codes, file_size)
+        print_stats()
         raise
-    print_codes(sum_codes, file_size)
-    
+    print_stats()
